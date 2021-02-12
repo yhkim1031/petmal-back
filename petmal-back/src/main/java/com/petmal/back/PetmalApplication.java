b03bbc280784c5e078fa3902f3c8e2fcd523eeec
김영훈 <yhkim1031@woowahan.com>
2021-02-13 02:30:48 +0900

    Initial commit

---
+++ petmal-back/src/main/java/com/petmal/back/PetmalApplication.java
@@ -0,0 +1,11 @@
+package com.petmal.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PetmalApplication.class,args);
+    }
+}
